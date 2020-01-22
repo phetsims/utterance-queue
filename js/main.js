0c7f7189eae99c568ae0c30edda86226013cffd8
phet-dev <phetadmin@gmail.com>
2020-01-22 04:02:43 -0700

    update copyright dates from daily grunt work

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Module that includes all Utterance Queue dependencies, so that requiring this module will return an object

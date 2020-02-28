3307606d30973bc8e382f1d784c8eee2eba2463d
phet-dev <phetadmin@gmail.com>
2020-02-28 03:55:44 -0700

    update copyright dates from daily grunt work

--- js/utteranceQueueNamespace.js
+++ js/utteranceQueueNamespace.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Creates the namespace for this repository.  By convention, this should have been declared in a file "utteranceQueue.js"

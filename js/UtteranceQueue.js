340adb47ed06885320d78ca798138a5604100754
samreid <reids@colorado.edu>
2020-09-30 09:36:34 -0600

    Move IO Type to core file, see https://github.com/phetsims/tandem/issues/212

--- js/UtteranceQueue.js
+++ js/UtteranceQueue.js
@@ -17,10 +17,13 @@ import stepTimer from '../../axon/js/stepTimer.js';
 import merge from '../../phet-core/js/merge.js';
 import PhetioObject from '../../tandem/js/PhetioObject.js';
 import Tandem from '../../tandem/js/Tandem.js';
+import BooleanIO from '../../tandem/js/types/BooleanIO.js';
+import IOType from '../../tandem/js/types/IOType.js';
+import StringIO from '../../tandem/js/types/StringIO.js';
+import VoidIO from '../../tandem/js/types/VoidIO.js';
 import AlertableDef from './AlertableDef.js';
 import AriaHerald from './AriaHerald.js';
 import Utterance from './Utterance.js';
-import UtteranceQueueIO from './UtteranceQueueIO.js';
 import utteranceQueueNamespace from './utteranceQueueNamespace.js';
 
 class UtteranceQueue extends PhetioObject {
@@ -37,7 +40,7 @@ class UtteranceQueue extends PhetioObject {
     if ( !implementAsSkeleton ) {
       superTypeOptions = {
         tandem: Tandem.GENERAL_VIEW.createTandem( 'utteranceQueue' ),
-        phetioType: UtteranceQueueIO,
+        phetioType: UtteranceQueue.UtteranceQueueIO,
         phetioState: false
       };
     }
@@ -374,6 +377,72 @@ class UtteranceQueue extends PhetioObject {
     return utteranceQueue;
   }
 }
+
+UtteranceQueue.UtteranceQueueIO = new IOType( 'UtteranceQueueIO', {
+  valueType: UtteranceQueue,
+  documentation: 'Manages a queue of Utterances that are read in order by a screen reader.',
+  events: [ 'announced' ],
+  methods: {
+    addToBack: {
+      returnType: VoidIO,
+      parameterTypes: [ StringIO ],
+      implementation: function( textContent ) {
+        return this.addToBack( textContent );
+      },
+      documentation: 'Add the utterance (string) to the end of the queue.',
+      invocableForReadOnlyElements: false
+    },
+
+    addToFront: {
+      returnType: VoidIO,
+      parameterTypes: [ StringIO ],
+      implementation: function( textContent ) {
+        return this.addToFront( textContent );
+      },
+      documentation: 'Add the utterance (string) to the beginning of the queue.',
+      invocableForReadOnlyElements: false
+    },
+
+    setMuted: {
+      returnType: VoidIO,
+      parameterTypes: [ BooleanIO ],
+      implementation: function( muted ) {
+        this.muted( muted );
+      },
+      documentation: 'Set whether the utteranceQueue will be muted or not. If muted, utterances still move through the ' +
+                     'queue but will not be read by screen readers.',
+      invocableForReadOnlyElements: false
+    },
+    getMuted: {
+      returnType: BooleanIO,
+      parameterTypes: [ VoidIO ],
+      implementation: function() {
+        return this.muted();
+      },
+      documentation: 'Get whether the utteranceQueue is muted. If muted, utterances still move through the ' +
+                     'queue but will not be read by screen readers.'
+    },
+    setEnabled: {
+      returnType: VoidIO,
+      parameterTypes: [ BooleanIO ],
+      implementation: function( enabled ) {
+        this.enabled( enabled );
+      },
+      documentation: 'Set whether the utteranceQueue will be enabled or not. When enabled, Utterances cannot be added to ' +
+                     'the queue, and the Queue cannot be cleared. Also nothing will be sent to assistive technology.',
+      invocableForReadOnlyElements: false
+    },
+    getEnabled: {
+      returnType: BooleanIO,
+      parameterTypes: [ VoidIO ],
+      implementation: function() {
+        return this.enabled();
+      },
+      documentation: 'Get whether the utteranceQueue is enabled. When enabled, Utterances cannot be added to ' +
+                     'the queue, and the Queue cannot be cleared. Also nothing will be sent to assistive technology.'
+    }
+  }
+} );
 
 utteranceQueueNamespace.register( 'UtteranceQueue', UtteranceQueue );
 export default UtteranceQueue;

a18d7b29ad230cf36b884c55a533f1c7fad083bc
Jesse <jesse.greenberg@colorado.edu>
2021-10-25 17:35:56 -0400

    typedoc for ResponsePacket

--- js/ResponsePacket.js
+++ js/ResponsePacket.js
@@ -1,7 +1,16 @@
 // Copyright 2021, University of Colorado Boulder
 
 /**
-
+ * The ResponsePacket collects the categories of a single "response" into the following sections:
+ * "Name Response" - A response that labels (names) some element to describe.
+ * "Object Response" - A response directly describing the state of the named element.
+ * "Context Response" - A response that describes surrounding context related to the named element or changes to it.
+ * "Hint Response" - A response that gives a hint about what en element is for or how to interact with it.
+ *
+ * Individual categories of responses can be enabled or disabled. The ResponsePacket keeps track of all these
+ * responses. When it is time to alert the responses of this ResponsePacket, the ResponseCollector will assemble
+ * a final string depending on which categories of responses are enabled.
+ *
  * @author Jesse Greenberg
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */

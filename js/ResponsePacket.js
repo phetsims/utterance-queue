4c8a5eeff7d48bbef6d0f725eabd282bdea1d7ef
zepumph <michael.kauzmann@colorado.edu>
2021-12-13 14:04:26 -0700

    Voicing should compose a ResponsePacket instead of duplicating those field implementations/documentations, https://github.com/phetsims/scenery/issues/1283

--- js/ResponsePacket.js
+++ js/ResponsePacket.js
@@ -7,6 +7,8 @@
  * "Context Response" - A response that describes surrounding context related to the named element or changes to it.
  * "Hint Response" - A response that gives a hint about what en element is for or how to interact with it.
  *
+ * A response is most often tied to an element, or an object that is being described/voiced.
+ *
  * Individual categories of responses can be enabled or disabled. The ResponsePacket keeps track of all these
  * responses. When it is time to alert the responses of this ResponsePacket, the ResponseCollector will assemble
  * a final string depending on which categories of responses are enabled.
@@ -54,11 +56,37 @@ class ResponsePacket {
     assert && assert( options.responsePatternCollection instanceof ResponsePatternCollection );
 
     // @public - mutate as needed until time to alert.
+
+
+    // @public {string|null} - The response to be spoken for this packet when speaking names. This is usually
+    // the same as the description accessible name, typically spoken on focus and on interaction, labelling what the
+    // object is. Mutate as needed until time to alert.
     this.nameResponse = options.nameResponse;
+
+    // @public {string|null} - The response to be spoken for this packet when speaking about object changes. This
+    // is usually the state information, such as the current input value. Mutate as needed until time to alert.
     this.objectResponse = options.objectResponse;
+
+    // @public {string|null} - The response to be spoken for this packet when speaking about context changes.
+    // This is usually a response that describes the surrounding changes that have occurred after interacting
+    // with the object. Mutate as needed until time to alert.
     this.contextResponse = options.contextResponse;
+
+    // @public {string|null} - The response to be spoken for this packet when speaking hints. This is usually the response
+    // that guides the user toward further interaction with this object if it is important to do so to use
+    // the application. Mutate as needed until time to alert.
     this.hintResponse = options.hintResponse;
+
+    // @public {boolean} - Controls whether or not the name, object, context, and hint responses are controlled
+    // by responseCollector Properties. If true, all responses will be spoken when requested, regardless
+    // of these Properties. This is often useful for surrounding UI components where it is important
+    // that information be heard even when certain responses have been disabled. Mutate as needed until time to alert.
     this.ignoreProperties = options.ignoreProperties;
+
+    // @public {ResponsePatternCollection} @public - A collection of response patterns that are used when consolidating
+    // each response with responseCollector. Controls the order of the Voicing responses and also punctuation
+    // used when responses are assembled into final content for the UtteranceQueue. See ResponsePatternCollection for
+    // more details. Mutate as needed until time to alert.
     this.responsePatternCollection = options.responsePatternCollection;
   }
 

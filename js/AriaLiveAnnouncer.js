782076ddec3a653953f19b2af14b4b8d721b6e9b
phet-dev <phetadmin@gmail.com>
2022-01-07 03:28:55 -0700

    update copyright dates from daily grunt work

--- js/AriaLiveAnnouncer.js
+++ js/AriaLiveAnnouncer.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * A static object used to send aria-live updates to a screen reader. These are alerts that are independent of user

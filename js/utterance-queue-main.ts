78b402a96963e3e0926f20cbb08a1d5f469a2206
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-02-28 16:50:04 -0700

    remove globals,  https://github.com/phetsims/utterance-queue/issues/105

--- js/utterance-queue-main.ts
+++ js/utterance-queue-main.ts
@@ -1,18 +1,5 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
-import axon from '../../axon/js/main.js'; // eslint-disable-line default-import-match-filename
-import phetCore from '../../phet-core/js/main.js'; // eslint-disable-line default-import-match-filename
-import utteranceQueue from './main.js'; // eslint-disable-line default-import-match-filename
-
-( function() {
-
-
-  // @ts-expect-error - Assigning to window to support standalone build.
-  window.axon = axon;
-
-  // @ts-expect-error - Assigning to window to support standalone build.
-  window.phetCore = phetCore;
-
-  // @ts-expect-error - Assigning to window to support standalone build.
-  window.utteranceQueue = utteranceQueue;
-} );
+import '../../axon/js/main.js';
+import '../../phet-core/js/main.js';
+import './main.js';

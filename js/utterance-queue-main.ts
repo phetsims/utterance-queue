3b465a0bc52158e17d19b4f3bc7e9a8b2b893cc9
phet-dev <phetadmin@gmail.com>
2023-03-01 05:31:00 -0700

    update copyright dates from daily grunt work

--- js/utterance-queue-main.ts
+++ js/utterance-queue-main.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 import '../../axon/js/main.js';
 import '../../phet-core/js/main.js';

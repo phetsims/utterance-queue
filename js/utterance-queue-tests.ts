6c198f2df1f6fa83f8d608e23b784381019bf29a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-05-10 12:31:59 -0600

    comment out test for testing, https://github.com/phetsims/aqua/issues/179

--- js/utterance-queue-tests.ts
+++ js/utterance-queue-tests.ts
@@ -8,7 +8,7 @@
 
 import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 import './ResponsePacketTests.js';
-import './UtteranceTests.js';
+// import './UtteranceTests.js'; TODO: bring back, https://github.com/phetsims/aqua/issues/179
 // import './UtteranceQueueTests.js'; TODO: bring back, https://github.com/phetsims/aqua/issues/179
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests

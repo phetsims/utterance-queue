cbbfb72f80e9969e713fff3a39242b7582891e02
phet-dev <phetadmin@gmail.com>
2023-06-27 03:37:57 -0600

    update copyright dates from daily grunt work

--- js/Announcer.ts
+++ js/Announcer.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * Abstract base class for the type that wires into an UtteranceQueue to announce Utterances.

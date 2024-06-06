e385ac0a6a83862b0f9895f76ec5375e730c49b9
Jesse <jesse.greenberg@colorado.edu>
2024-06-06 12:25:26 -0400

    Rename a11y to pdom, see https://github.com/phetsims/sun/issues/328

--- js/ValueChangeUtterance.ts
+++ js/ValueChangeUtterance.ts
@@ -20,7 +20,7 @@ export type ValueChangeUtteranceOptions = UtteranceOptions;
 class ValueChangeUtterance extends Utterance {
 
   public constructor( providedOptions?: ValueChangeUtteranceOptions ) {
-    deprecationWarning( 'Please use AccessibleValueHander.a11yCreateContextResponseAlert(), see https://github.com/phetsims/sun/issues/685' );
+    deprecationWarning( 'Please use AccessibleValueHander.pdomCreateContextResponseAlert(), see https://github.com/phetsims/sun/issues/685' );
 
     const options = optionize<ValueChangeUtteranceOptions, EmptySelfOptions, UtteranceOptions>()( {
 

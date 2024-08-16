ee4d5ce6849b90ed9cc7869e4673fcba9780df86
phet-dev <phetadmin@gmail.com>
2024-08-16 03:27:52 -0600

    update copyright dates from daily grunt work

--- js/UtteranceQueue.ts
+++ js/UtteranceQueue.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * Manages a queue of Utterances that are read in order by assistive technology (AT). This queue typically reads

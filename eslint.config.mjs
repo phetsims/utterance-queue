cad2e63d6c536a79bae6d126808b2133e93f14eb
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:02 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for utterance-queue.
+ * ESLint configuration for utterance-queue.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)

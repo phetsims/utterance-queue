a5a8e1b22842165b53490cbef143909d32bb2cf8
Sam Reid <reids@colorado.edu>
2025-02-06 13:08:33 -0700

    Allow importing directly, see https://github.com/phetsims/scenery/issues/1683

--- js/AriaLiveAnnouncer.ts
+++ js/AriaLiveAnnouncer.ts
@@ -32,7 +32,6 @@ import Enumeration from '../../phet-core/js/Enumeration.js';
 import EnumerationValue from '../../phet-core/js/EnumerationValue.js';
 import optionize from '../../phet-core/js/optionize.js';
 import platform from '../../phet-core/js/platform.js';
-// eslint-disable-next-line phet/bad-sim-text
 import PDOMUtils from '../../scenery/js/accessibility/pdom/PDOMUtils.js';
 import Announcer, { AnnouncerAnnounceOptions, AnnouncerOptions } from './Announcer.js';
 import { ResolvedResponse } from './ResponsePacket.js';

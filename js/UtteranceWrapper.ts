1294809e5cc582aeb9b647219e1e4c17f708ba5b
phet-dev <phetadmin@gmail.com>
2025-12-17 05:39:30 -0700

    update copyright dates from daily grunt work

--- js/UtteranceWrapper.ts
+++ js/UtteranceWrapper.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * A type to wrap an Utterance while in the UtteranceQueue, see UtteranceQueue for implementation. Internal to
